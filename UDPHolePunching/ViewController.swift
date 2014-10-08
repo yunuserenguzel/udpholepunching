6a43c5c199d2a16e5577288e4bc796a3ba2b2de1
Yunus Eren Guzel <exculuber@gmail.com>
2014-10-08 13:52:54 +0300

    Initial Commit

---
+++ UDPHolePunching/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UDPHolePunching
+//
+//  Created by Yunus Eren Guzel on 08/10/14.
+//  Copyright (c) 2014 Yunus Eren Guzel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
